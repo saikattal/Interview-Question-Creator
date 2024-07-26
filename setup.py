3d8048da0ff05a1932418c40f400ab1e7a8dcf1e
Saikat T <saikat07taluk@gmail.com>
2024-07-26 22:27:42 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.1',
+    author="Saikat T",
+    author_email="saikat07taluk@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
